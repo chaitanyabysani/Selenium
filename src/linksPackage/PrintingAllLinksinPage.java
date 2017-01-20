281f646e2e695c04e133c809567758e858db92ce
Chaitanya <Chaitanya@Chaitanya-PC>
2017-01-20 09:18:07 +0530

    Links Module package

---
+++ src/linksPackage/PrintingAllLinksinPage.java
@@ -0,0 +1,31 @@
+package linksPackage;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class PrintingAllLinksinPage {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver", "H:\\Jar files\\Gecko Drivers\\geckodriver-v0.13.0-win64\\geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("https://www.wikipedia.org");
+		
+		System.out.println("------------Printing all links in page");
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println(links.size());
+		
+		for(int i=0; i<links.size(); i++)
+		{
+			System.out.println(links.get(i).getAttribute("href"));
+		}
+		
+		System.out.println("Total links are : " +links.size());
+
+	}
+
+}
